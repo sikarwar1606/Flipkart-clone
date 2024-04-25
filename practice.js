8c1c8273fce03b6f1e9029da5921560bf42f20b5
Anupam <sikarwara1606@gmail.com>
2024-04-25 23:28:46 +0530

    Added electronic section again, with a new style

---
+++ practice.js
@@ -0,0 +1,52 @@
+// const anupam = {
+//     fullName: "Anupam Singh Sikarwar",
+//     age: 23,
+//     status: "Single",
+// }
+
+// student[]
+// console.log(anupam.age);
+
+
+// const student = {
+//     Name: 'Anupam Singh Sikarwar',
+//     Age: 23,
+//     Cast: 'Rajput',
+//     Roll_number: '142025',
+
+// }
+
+
+// const secure = {
+//     emp: 200,
+//     machines: 20,
+//     area: 200,
+// }
+
+// console.log("secure");
+
+
+const a=5;
+const b=3;
+
+
+    const cond1= a==5;
+const cond2= b==3;
+
+console.log ("cond1 && cond2= ", cond1 && cond2);
+
+
+const cond1= a>5;
+const cond2= b==3;
+console.log ("cond1 && cond2= ", cond1 && cond2);
+
+const cond1= a==5;
+const cond2= b!=3;
+console.log ("cond1 && cond2= ", cond1 && cond2);
+
+
+const cond1= a<5;
+const cond2= b<3;
+console.log ("cond1 && cond2= ", cond1 && cond2);
+
+
